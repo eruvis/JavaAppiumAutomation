a94efb6f0cb84f3f460bf9f316f6e0c382ab19b5
eruvis <eruvis@mail.ru>
2022-01-10 21:06:47 +0700

    code from lesson 5.4

--- src/lib/ui/SearchPageObject.java
+++ src/lib/ui/SearchPageObject.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.By;
 
 public class SearchPageObject extends MainPageObject {
     private static final String
-            SEARCH_INIT_ELEMENT = "//*[contains(@text, 'Search Wikipedia')]",
+            SEARCH_INIT_ELEMENT = "org.wikipedia:id/search_container",
             SEARCH_INPUT = "//*[contains(@text, 'Search…')]",
+            SEARCH_CANCEL_BUTTON = "org.wikipedia:id/search_close_btn",
             SEARCH_RESULT_BY_SUBSTRING_TPL = "//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']";
 
     public SearchPageObject(AppiumDriver driver) {
@@ -20,8 +21,20 @@ public class SearchPageObject extends MainPageObject {
     /* TEMPlATES METHODS */
 
     public void initSearchInput() {
-        this.waitForElementPresent(By.xpath(SEARCH_INIT_ELEMENT),"Cannot find search init element");
-        this.waitForElementAndClick(By.xpath(SEARCH_INIT_ELEMENT), "Cannot find and click search init element", 5);
+        this.waitForElementPresent(By.id(SEARCH_INIT_ELEMENT),"Cannot find search init element");
+        this.waitForElementAndClick(By.id(SEARCH_INIT_ELEMENT), "Cannot find and click search init element", 5);
+    }
+
+    public void waitForCancelButtonToAppear() {
+        this.waitForElementPresent(By.id(SEARCH_CANCEL_BUTTON), "Cannot find search cancel button", 5);
+    }
+
+    public void waitForCancelButtonToDisappear() {
+        this.waitForElementNotPresent(By.id(SEARCH_CANCEL_BUTTON), "Search cancel button is still present!", 5);
+    }
+
+    public void clickCancelSearch() {
+        this.waitForElementAndClick(By.id(SEARCH_CANCEL_BUTTON), "Cannot find and click search cancel button", 5);
     }
 
     public void typeSearchLine(String searchLine) {
