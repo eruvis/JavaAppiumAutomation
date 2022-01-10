db6fe667da1317d825531d4f3634536a72d2135a
eruvis <eruvis@mail.ru>
2022-01-10 20:06:10 +0700

    code from lesson 5.1

---
+++ src/lib/CoreTestCase.java
@@ -0,0 +1,42 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class CoreTestCase extends TestCase {
+    protected AppiumDriver driver;
+    private final String AppiumURL = "http://127.0.0.1:4723/wd/hub";
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("platformVersion", "8.1.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "C:/Users/Eruvis/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+
+        if (driver.getOrientation() == ScreenOrientation.LANDSCAPE) {
+            driver.rotate(ScreenOrientation.PORTRAIT);
+        }
+
+    }
+
+    @Override
+    public void tearDown() throws Exception{
+        driver.quit();
+
+        super.tearDown();
+    }
+}
