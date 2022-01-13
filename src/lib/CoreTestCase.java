10928117ab080fe57670098f3c51b454dcf16247
eruvis <eruvis@mail.ru>
2022-01-13 21:16:28 +0700

    code from lesson 6.5

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -9,32 +9,24 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.URL;
 
 public class CoreTestCase extends TestCase {
+
+    private static final String APPIUM_URL = "http://127.0.0.1:4723/wd/hub";
+    private static final String PLATFORM_ANDROID = "android";
+    private static final String PLATFORM_IOS = "ios";
+
     protected AppiumDriver driver;
-    private final String AppiumURL = "http://127.0.0.1:4723/wd/hub";
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "emulator-5554");
-        capabilities.setCapability("platformVersion", "8.1.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "C:/Users/Eruvis/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
-
-        if (driver.getOrientation() == ScreenOrientation.LANDSCAPE) {
-            this.rotateScreenPortrait();
-        }
-
+        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
+        driver = new AndroidDriver(new URL(APPIUM_URL), capabilities);
+        this.rotateScreenPortrait();
     }
 
     @Override
-    protected void tearDown() throws Exception{
+    protected void tearDown() throws Exception {
         driver.quit();
 
         super.tearDown();
@@ -50,5 +42,28 @@ public class CoreTestCase extends TestCase {
 
     protected void backgroundApp(int seconds) {
         driver.runAppInBackground(seconds);
+    }
+
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
+        String platform = System.getenv("PLATFORM");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("deviceName", "emulator-5554");
+            capabilities.setCapability("platformVersion", "8.1.0");
+            capabilities.setCapability("automationName", "Appium");
+            capabilities.setCapability("appPackage", "org.wikipedia");
+            capabilities.setCapability("appActivity", ".main.MainActivity");
+            capabilities.setCapability("app", "C:/Users/Eruvis/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        } else if (platform.equals(PLATFORM_IOS)) {
+            capabilities.setCapability("platformName", "iOS");
+            capabilities.setCapability("deviceName", "iPhone SE");
+            capabilities.setCapability("platformVersion", "11.3");
+            capabilities.setCapability("app", "Users/Eruvis/Desktop/JavaAppiumAutomation/apks/Wikipedia.app");
+        } else {
+            throw new Exception("Cannot get run platform from env variables. Platform value: " + platform);
+        }
+        return capabilities;
     }
 }
