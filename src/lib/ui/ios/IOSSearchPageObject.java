940721cd954b431f08cc031153b5e4cf53087895
eruvis <eruvis@mail.ru>
2022-01-15 18:12:53 +0700

    ex.12

--- src/lib/ui/ios/IOSSearchPageObject.java
+++ src/lib/ui/ios/IOSSearchPageObject.java
@@ -9,7 +9,7 @@ public class IOSSearchPageObject extends SearchPageObject {
         SEARCH_INPUT = "xpath://SCUIElementTypeSearchField[@value='Search Wikipedia']";
         SEARCH_CANCEL_BUTTON = "id:Close";
         SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://XCUIElementTypeLink[contains(@name,'{SUBSTRING}')]";
-        SEARCH_RESULT_BY_TWO_SUBSTRING_TPL = "?";
+        SEARCH_RESULT_BY_TWO_SUBSTRING_TPL = "xpath://XCUIElementTypeLink[@name='{FIRST_SUBSTRING}" + " " + "{SECOND_SUBSTRING}']";
         SEARCH_RESULT_ELEMENT = "xpath:XCUIElementTypeLink";
         SEARCH_RESULT_FRAGMENT = "?";
         SEARCH_EMPTY_RESULT_ELEMENT = "xpath://XCUIElementTypeStaticText[@name='No results found']";
